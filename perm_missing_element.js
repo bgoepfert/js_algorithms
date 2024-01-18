7a540a2902b5d0a01c1d402691c21a0a7ef75f44
Bastien Goepfert <bas.goepfert@gmail.com>
2024-01-18 19:31:08 +0200

    Add problem description to perm_missing_element

--- perm_missing_element.js
+++ perm_missing_element.js
@@ -1,3 +1,9 @@
+// An array A consisting of N different integers is given.
+// The array contains integers in the range [1..(N + 1)],
+// which means that exactly one element is missing.
+
+// Your goal is to find that missing element.
+
 function findMissingElementInRange(A) {
   const correctSum = (A.length * (A.length + 1)) / 2;
   let incorrectSum = 0;
